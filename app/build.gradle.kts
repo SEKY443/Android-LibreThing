1102765f1061b116f9f52b16ec044b578835bff3
SEKY443 <47088069+SEKY443@users.noreply.github.com>
2026-08-25 10:56:10 +0800

    Bump version to 1.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "io.github.seky443.librething"
         minSdk = 24
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.2.1"
+        versionCode = 5
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
